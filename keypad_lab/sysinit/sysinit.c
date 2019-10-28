17e1dc6cff86521d3315dfda526e11b847265276
HagerDakroury <37930475+HagerDakroury@users.noreply.github.com>
2019-10-28 15:54:05 +0200

    INITIALIZATION OF KEYPAD

--- keypad_lab/sysinit/sysinit.c
+++ keypad_lab/sysinit/sysinit.c
@@ -1,13 +1,14 @@
 #include "sysinit.h"
 #include "tm4c123gh6pm.h"
 #include "src/timer/timer0.h"
+
 void sys_init(){
 
     //ENABLING CLOCK AND DELAY
     SYSCTL_RCGCGPIO_R |= (SYSCTL_RCGCGPIO_R1 | SYSCTL_RCGCGPIO_R5);
     delay(300);
 
-    //PORT F CONFIG
+ /*   //PORT F CONFIG
     GPIO_PORTF_LOCK_R |=  GPIO_LOCK_KEY;
     GPIO_PORTF_CR_R |= 1;
     GPIO_PORTF_DEN_R |= 0x11;
@@ -15,10 +16,22 @@ void sys_init(){
     GPIO_PORTF_DIR_R &= ~0x11;
     GPIO_PORTF_DIR_R |= 0b10;
     GPIO_PORTF_DATA_R  |= 0x2;
+    */
 
     //PORT B CONFIG
     GPIO_PORTB_DEN_R |= 0xff;
     GPIO_PORTB_DIR_R |= 0xff;
+
+    //PORT E CONFIG as KEYPAD input pins
+    GPIO_PORTE_DEN_R |= 0xf;
+    GPIO_PORTE_DIR_R &= ~0xf;
+    GPIO_PORTE_PUR_R |= (C0|C1|C2|C3);
+
+   //PORT D CONFIG as KEPAD output pins
+    GPIO_PORTD_DEN_R |= 0xf;
+    GPIO_PORTD_DIR_R |= 0xf;
+
+
 }
 
 void sys_start(){
